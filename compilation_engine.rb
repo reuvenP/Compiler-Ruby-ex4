5af1cc57ee409b0cd34b07ea667fda9605acd947
reuvenp <reuvenp@checkpoint.com>
2017-01-03 20:28:54 +0200

    start part 2

--- compilation_engine.rb
+++ compilation_engine.rb
@@ -1,13 +1,20 @@
 require './JackTokenizer'
+
 class CompilationEngine
 
   def initialize(path)
     JackTokenizer.new(path)
     @all_files = Dir.entries(path).select{|f| f.end_with? 'T.xml'}
     @all_files.each {|file|
-      puts path + "\\" + file
+      tokens_stream = File.read(path + "\\" + file)
+      tree = compile_class(tokens_stream)
     }
   end
+
+  def compile_class(tokens) #Compiles a complete class.
+
+  end
+
 end
 
 CompilationEngine.new('C:\Users\reuvenp\Downloads\compilers\ex4\compiled\ArrayTest')
